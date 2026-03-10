149482ec8e9f71ea71a3ac75b71dd578db4cec27
Miguel Zarabanda <miguel.zarabanda@urosario.edu.co>
2026-03-10 23:37:46 +0000

    Agregar validación para códigos de tickets VIP y nuevos scripts de estadísticas

--- make-cpp-event-app/src/validator.cpp
+++ make-cpp-event-app/src/validator.cpp
@@ -9,6 +9,12 @@ vector<string> validateTicket(const Ticket& ticket) {
 
     regex codePattern("^TK\\d{3}$");
 
+    int number = stoi(ticket.code.substr(2)); 
+
+    if (ticket.type == "vip" && number <= 500) {
+        errors.push_back("VIP tickets must have code greater than TK500");
+    }
+
     if (ticket.code.empty()) {
         errors.push_back("Empty ticket code");
     }
